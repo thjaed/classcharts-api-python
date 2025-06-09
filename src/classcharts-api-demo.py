2132d8fee0c7751f8fc0f128d52386b01e9d7db9
thjaed <thomasjedwards10@gmail.com>
2025-06-09 19:44:40 +0100

    classcharts api

---
+++ src/classcharts-api-demo.py
@@ -0,0 +1,20 @@
+from classcharts_api import StudentClient
+from datetime import datetime
+
+code = input("Enter your ClassCharts Pupil Code: ")
+dob = input("Enter your date of birth in the format YYYY-MM-DD: ")
+
+client = StudentClient(code=code, dob=dob)
+
+name = client.get_account_data()["data"]["user"]["first_name"]
+print(f"Hi {name}!")
+
+lessons = client.get_lessons()["data"]
+print("---YOUR TIMETABLE FOR TODAY---")
+for lesson in lessons:
+    start_time = datetime.fromisoformat(lesson["start_time"])
+    subject = lesson["subject_name"]
+    teacher = lesson["teacher_name"]
+    room = lesson["room_name"]
+    print(f"{start_time.strftime("%H:%M")}: {subject} with {teacher} in {room}")
+print("------------------------------")
